ffcb3adb264e2177e917b75e5c7a3ad7e972fbf2
Kate Starykh <folk.alexsandrov@mail.ru>
2023-11-07 15:05:44 +0300

    First commit

--- OOP.py
+++ OOP.py
@@ -2,20 +2,18 @@ from typing import Literal
 class Animal:
     def __init__(
             self,
-            animal_type:str,
-            animal_noises:str,
-            animal_sound:str,
-            feeding:Literal ["травоядное", "хищное", "всеядное"]
+            animal_type: str,
+            animal_sound: str,
+            feeding: Literal ["травоядное", "хищное", "всеядное"]
                 ):
         self.animal_type = animal_type
-        self.animal_noises = animal_noises
         self.animal_sound = animal_sound
         self.feeding = feeding
 
     def __str__(self)->str:
         return f"Это {self.feeding} животное {self.animal_type}."
     def sound(self) -> str:
-        return f"{self.animal_type} издает следующие звуки {self.animal_noises}."
+        return f"{self.animal_type} издает следующие звуки {self.animal_sound}."
     
 cat = Animal ("Кот", "мяу", "всеядное")
 giraf = Animal ("жираф", "х-ы-ы", "травоядное")
